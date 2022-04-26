1682dd26a2ff7dbf65dc341b39d88de4f9345fea
agndiaye001 <mamindiaye0101@gmail.com>
2022-04-26 23:09:00 +0200

    "Installationand sting up file and database"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // nb migration
-        Schema::defaultStringLength(1911);
+       Schema::defaultStringLength(1911);
     }
 }
